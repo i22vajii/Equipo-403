c9c2760f162f1e34a23c21e4281bfb6033d1a1d9
i22vajii <150832700+i22vajii@users.noreply.github.com>
2023-12-11 12:16:42 +0000

    aumentado menu

--- menus.cc
+++ menus.cc
@@ -39,10 +39,7 @@ void menu_visitante(){
     int opcvisit=0;
     do{
     std::cout<<"Elija una opcion"<<std:: endl;
-    std::cout<<"1. f1"<<std:: endl;
-    std::cout<<"2. f2"<<std:: endl;
-    std::cout<<"3. f3"<<std:: endl;
-    std::cout<<"4. f4"<<std:: endl;
+    std::cout<<"1. Ver actividades"<<std:: endl;
     std::cout<<"0. Salir:"<<std:: endl;
     std:: cin>>opcvisit;
     system("clear");
@@ -51,16 +48,7 @@ void menu_visitante(){
             std::cout<<"Saliendo del programa"<<std::endl;
             break;
         case 1:
-            //funcion
-            break;
-        case 2:
-            //funcion
-            break;
-        case 3:
-            //funcion
-            break;
-        case 4:
-            //funcion
+            //VerActiviades();
             break;
         default :
             std::cout<<"Opcion incorrecta la opcion debe estar comprendida entre 0 y 4"<<std:: endl;
@@ -74,10 +62,11 @@ void menu_usuario(){
     int opcuser=0;
     do{
     std::cout<<"Elija una opcion"<<std:: endl;
-    std::cout<<"1. f1"<<std:: endl;
-    std::cout<<"2. f2"<<std:: endl;
-    std::cout<<"3. f3"<<std:: endl;
-    std::cout<<"4. f4"<<std:: endl;
+    std::cout<<"1. Ver actividades"<<std:: endl;
+    std::cout<<"2. Hacer preisncripcion"<<std:: endl;
+    std::cout<<"3. Cancelar preisncripcion"<<std:: endl;
+    std::cout<<"4. Pago"<<std:: endl;
+    std::cout<<"5. Devolucion de pago"<<std::endl;
     std::cout<<"0. Salir:"<<std:: endl;
     std:: cin>>opcuser;
     system("clear");
@@ -86,15 +75,18 @@ void menu_usuario(){
             std::cout<<"Saliendo del programa"<<std::endl;
             break;
         case 1:
-            //funcion
+            //VeraActividades();
             break;
         case 2:
-            //funcion
+            //PreisncribrirUsuario();
             break;
         case 3:
-            //funcion
+            //CancelarPreisncripcion();
             break;
         case 4:
+            //Pago();
+            break;
+        case 5:
             //funcion
             break;
         default:
@@ -109,7 +101,7 @@ void menu_organizador(){
     int opcorganizador=0;
     do{
     std::cout<<"Elija una opcion"<<std:: endl;
-    std::cout<<"1. f1"<<std:: endl;
+    std::cout<<"1. "<<std:: endl;
     std::cout<<"2. f2"<<std:: endl;
     std::cout<<"3. f3"<<std:: endl;
     std::cout<<"4. f4"<<std:: endl;
@@ -144,10 +136,16 @@ void menu_director(){
     int opcdirector=0;
     do{
     std::cout<<"Elija una opcion"<<std:: endl;
-    std::cout<<"1. f1"<<std:: endl;
-    std::cout<<"2. f2"<<std:: endl;
-    std::cout<<"3. f3"<<std:: endl;
-    std::cout<<"4. f4"<<std:: endl;
+    std::cout<<"1. Ver actividades"<<std:: endl;
+    std::cout<<"2. Hacer preisncripcion"<<std:: endl;//pensar
+    std::cout<<"3. Cancelar preisncripcion"<<std:: endl;
+    std::cout<<"4. Pago"<<std:: endl;
+    std::cout<<"5. Devolucion de pago"<<std::endl;//hasta aqui
+    std::cout<<"6. Aprobar preisncripciones"<<std:: endl;
+    std::cout<<"7. Actualizar Actividad"<<std:: endl;
+    std::cout<<"8. Aprobar pago"<<std:: endl;
+    std::cout<<"9. "<<std:: endl;
+    std::cout<<"5. "<<std::endl;
     std::cout<<"0. Salir:"<<std:: endl;
     std:: cin>>opcdirector;
     system("clear");
